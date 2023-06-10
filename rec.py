03dd5afc52a47dec6267228f9adaeeea9f372f3b
RaniaAmor <95028742+RaniaAmor@users.noreply.github.com>
2023-06-10 12:55:12 +0400

    rec.py

--- rec.py
+++ rec.py
@@ -1,2 +1,5 @@
+import genres.py
+
 favorite_genre = input("Please enter your favorite book genre: ")
-print("Your favorite genre is:", favorite_genre)
+recommendation = book_recommendations.get_book_recommendation(favorite_genre)
+print("Book Recommendation:", recommendation)
