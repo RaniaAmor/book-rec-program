7456767f12a94dc39b0dab55db8dd74e43477fbb
RaniaAmor <95028742+RaniaAmor@users.noreply.github.com>
2023-06-10 13:02:26 +0400

    genres.py

--- genres.py
+++ genres.py
@@ -19,25 +19,25 @@ genres = ['Fiction',
 'Children Literature',
 'Young Adult', ]
 
-book_data=[ ['Fiction:', "To Kill a Mockingbird", 'by Harper Lee']
-           ['Non-Fiction:', "Sapiens: A Brief History of Humankind",  'by Yuval Noah Harari']
-           ['Mystery:' ,"Gone Girl" ,'by Gillian Flynn']
-           ['Thriller:' ,"The Girl on the Train", 'by Paula Hawkins']
-           ['Romance:', "Pride and Prejudice" ,'by Jane Austen']
-           ['Science Fiction:' ,"Dune", 'by Frank Herbert']
-           ['Fantasy:' ,"The Hobbit", 'by J.R.R. Tolkien']
-           ['Horror:', "The Shining", 'by Stephen King']
-           ['Historical Fiction:', "The Book Thief", 'by Markus Zusak']
-           ['Biography:', "The Diary of a Young Girl" ,'by Anne Frank']
-           ['Autobiography:',"The Autobiography of Malcolm X" ,'by Malcolm X and Alex Haley']
-           ['Memoir:', "Educated" ,'by Tara Westover']
-           ['Self-Help:', "The 7 Habits of Highly Effective People", 'by Stephen R. Covey']
-           ['Travel:', "Eat, Pray, Love", 'by Elizabeth Gilbert']
-           ['Poetry:', "Milk and Honey", 'by Rupi Kaur']
-           ['Drama:', "Romeo and Juliet" ,'by William Shakespeare']
-           ['Comedy:',"The Hitchhiker's Guide to the Galaxy" ,'by Douglas Adams']
-           ['Satire:', "Animal Farm" ,'by George Orwell']
-           ['Childrens Literature:', "Harry Potter and the Sorcerers Stone", 'by J.K. Rowling']
+book_data=[ ['Fiction:', "To Kill a Mockingbird", 'by Harper Lee'],
+           ['Non-Fiction:', "Sapiens: A Brief History of Humankind",  'by Yuval Noah Harari'],
+           ['Mystery:' ,"Gone Girl" ,'by Gillian Flynn'],
+           ['Thriller:' ,"The Girl on the Train", 'by Paula Hawkins'],
+           ['Romance:', "Pride and Prejudice" ,'by Jane Austen'],
+           ['Science Fiction:' ,"Dune", 'by Frank Herbert'],
+           ['Fantasy:' ,"The Hobbit", 'by J.R.R. Tolkien'],
+           ['Horror:', "The Shining", 'by Stephen King'],
+           ['Historical Fiction:', "The Book Thief", 'by Markus Zusak'],
+           ['Biography:', "The Diary of a Young Girl" ,'by Anne Frank'],
+           ['Autobiography:',"The Autobiography of Malcolm X" ,'by Malcolm X and Alex Haley'],
+           ['Memoir:', "Educated" ,'by Tara Westover'],
+           ['Self-Help:', "The 7 Habits of Highly Effective People", 'by Stephen R. Covey'],
+           ['Travel:', "Eat, Pray, Love", 'by Elizabeth Gilbert'],
+           ['Poetry:', "Milk and Honey", 'by Rupi Kaur'],
+           ['Drama:', "Romeo and Juliet" ,'by William Shakespeare'],
+           ['Comedy:',"The Hitchhiker's Guide to the Galaxy" ,'by Douglas Adams'],
+           ['Satire:', "Animal Farm" ,'by George Orwell'],
+           ['Childrens Literature:', "Harry Potter and the Sorcerers Stone", 'by J.K. Rowling'],
            ['Young Adult (YA):', "The Fault in Our Stars" ,'by John Green']
            ]
 def get_book_recommendation(genre):
