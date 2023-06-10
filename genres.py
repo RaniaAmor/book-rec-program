0de0c4de8145cfd7db564029f40a363035ef26b5
RaniaAmor <95028742+RaniaAmor@users.noreply.github.com>
2023-06-10 12:32:43 +0400

    Add files via upload

---
+++ genres.py
@@ -0,0 +1,44 @@
+genres = ['Fiction',
+'Non-Fiction',
+'Mystery',
+'Thriller',
+'Romance',
+'Science Fiction',
+'Fantasy',
+'Horror',
+'Historical Fiction',
+'Biography',
+'Autobiography',
+'Memoir',
+'Self-Help',
+'Travel',
+'Poetry',
+'Drama',
+'Comedy',
+'Satire',
+'Children Literature',
+'Young Adult', ]
+
+book_data=[ ['Fiction:', "To Kill a Mockingbird", 'by Harper Lee']
+           ['Non-Fiction:', "Sapiens: A Brief History of Humankind",  'by Yuval Noah Harari']
+           ['Mystery:' ,"Gone Girl" ,'by Gillian Flynn']
+           ['Thriller:' ,"The Girl on the Train", 'by Paula Hawkins']
+           ['Romance:', "Pride and Prejudice" ,'by Jane Austen']
+           ['Science Fiction:' ,"Dune", 'by Frank Herbert']
+           ['Fantasy:' ,"The Hobbit", 'by J.R.R. Tolkien']
+           ['Horror:', "The Shining", 'by Stephen King']
+           ['Historical Fiction:', "The Book Thief", 'by Markus Zusak']
+           ['Biography:', "The Diary of a Young Girl" ,'by Anne Frank']
+
+           ['Autobiography:',"The Autobiography of Malcolm X" ,'by Malcolm X and Alex Haley']
+           ['Memoir:', "Educated" ,'by Tara Westover']
+           ['Self-Help:', "The 7 Habits of Highly Effective People", 'by Stephen R. Covey']
+           ['Travel:', "Eat, Pray, Love", 'by Elizabeth Gilbert']
+           ['Poetry:', "Milk and Honey", 'by Rupi Kaur']
+           ['Drama:', "Romeo and Juliet" ,'by William Shakespeare']
+           ['Comedy:',"The Hitchhiker's Guide to the Galaxy" ,'by Douglas Adams']
+           ['Satire:', "Animal Farm" ,'by George Orwell']
+           ['Childrens Literature:', "Harry Potter and the Sorcerers Stone", 'by J.K. Rowling']
+           ['Young Adult (YA):', "The Fault in Our Stars" ,'by John Green']
+        
+           ]
